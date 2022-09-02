87bfd5290c81b8be54a1257ae2df2327278e18eb
geunsoo96 <geunsoo1106@gmail.com>
2022-09-02 16:49:50 +0900

    클릭 이벤트 무한반복 하는법 배움

--- developTraining.js
+++ developTraining.js
@@ -23,3 +23,62 @@ bottomButton.addEventListener("click", function () {
   secondContent.style.display = "none";
   thirdContent.style.display = "flex";
 });
+
+// -------------------------------------
+
+const root = document.getElementById("root");
+const option = document.getElementById("option");
+const head = document.getElementById("head");
+const menu = document.querySelectorAll("a");
+const write = document.getElementsByClassName("write");
+let on = document.getElementById("option").children[0];
+let off = document.getElementById("option").children[1];
+console.dir(write);
+let isChange = false;
+
+option.addEventListener("click", function () {
+  if (isChange === false) {
+    root.style.backgroundColor = "#555";
+    option.style.backgroundColor = "white";
+    option.style.color = "#555";
+    for (let value of write) {
+      value.style.color = "white";
+    }
+    on.style.display = "none";
+    off.style.display = "block";
+    head.style.borderBottom = "2px solid white";
+    for (let i = 0; i < menu.length; i++) {
+      menu[i].style.color = "white";
+    }
+    isChange = true;
+  } else {
+    root.style.backgroundColor = "beige";
+    option.style.backgroundColor = "#555";
+    option.style.color = "white";
+    on.style.display = "block";
+    off.style.display = "none";
+    head.style.borderBottom = "2px solid #555";
+    for (let value of write) {
+      value.style.color = "#555";
+    }
+    for (let i = 0; i < menu.length; i++) {
+      menu[i].style.color = "#555";
+    }
+    isChange = false;
+  }
+});
+
+// let onMode =
+
+let offMode = (root.style.backgroundColor = "beige");
+option.style.backgroundColor = "#555";
+option.style.color = "white";
+on.style.display = "block";
+off.style.display = "none";
+head.style.borderBottom = "2px solid #555";
+for (let value of write) {
+  value.style.color = "#555";
+}
+for (let i = 0; i < menu.length; i++) {
+  menu[i].style.color = "#555";
+}
